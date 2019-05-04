1ee6f9ff24031405e68c3b3ae54cc7b0eb91f0a3
Kostoeva <irmareka@gmail.com>
2019-05-04 11:01:37 -0700

    adding skybox again

---
+++ Space_Race/Assets/Scripts/PlayVideo.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayVideo : MonoBehaviour {
+
+    public GameObject videoPlayer;
+    public int timeTillStop;
+
+	// Use this for initialization
+	void Start () {
+        // Prevent from playing right away
+        videoPlayer.SetActive(false);
+		
+	}
+	
+	// On collision
+	void OnTriggerEnter (Collider player)
+    {
+        // If collided with "Player"
+        if (player.gameObject.tag == "Player")
+        {
+            Debug.Log("player collided with Gagarin");
+            // Start video.
+            videoPlayer.SetActive(true);
+            Destroy(videoPlayer, timeTillStop);
+        }
+    }
+}
