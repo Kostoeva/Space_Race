49cda0a9cf9ee6e1967be1967d4780b993591e52
Kostoeva <irmareka@gmail.com>
2019-05-04 11:34:11 -0700

    adding standard assets - untility, fps, crossplat

---
+++ Space_Race/Assets/Scripts/SputnikOnTrigger.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SputnikOnTrigger : MonoBehaviour {
+
+    public GameObject textObject;
+
+	// Use this for initialization
+	void Start () {
+        textObject.SetActive(false);
+	}
+	
+	void OnTriggerEnter (Collider player)
+    {
+        if (player.gameObject.tag == "Player")
+        {
+            Debug.Log("player collided with sputnik");
+
+            textObject.SetActive(true);
+            StartCoroutine("Wait");
+        }
+    }
+
+    IEnumerator Wait()
+    {
+        yield return new WaitForSeconds(5);
+        textObject.SetActive(false);
+    }
+}
