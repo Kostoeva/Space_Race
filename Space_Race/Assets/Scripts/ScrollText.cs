5fd25f6b133c9a70b7926f04fe62b0c4135c22a9
Kostoeva <irmareka@gmail.com>
2019-05-07 12:59:21 -0700

    adding intro scene

---
+++ Space_Race/Assets/Scripts/ScrollText.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollText : MonoBehaviour {
+
+    public float speed = 20;
+	
+	// Update is called once per frame
+	void Update () {
+        // Get current pos of parent.
+        Vector3 currPos = transform.position;
+
+        // Get vector direction.
+        Vector3 localDirectionUp = transform.TransformDirection(0, 1, 0);
+
+        // Scroll text upward.
+        currPos += localDirectionUp * speed * Time.deltaTime;
+        transform.position = currPos;
+	}
+}
