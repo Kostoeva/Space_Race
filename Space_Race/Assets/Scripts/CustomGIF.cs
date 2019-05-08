8b7a6eb3368da8484e97bb04120b936ad4361424
Kostoeva <irmareka@gmail.com>
2019-05-07 19:33:25 -0700

    adding audio to laika

---
+++ Space_Race/Assets/Scripts/CustomGIF.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CustomGIF : MonoBehaviour {
+
+    // Use this for initialization
+    void Start () {
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        StartCoroutine(GIF());
+    }
+
+    IEnumerator GIF()
+    {
+        int ind = 0;
+        string name = "laika-gif/frame_00_delay-0.1s";
+        for (int i = 0; i < 10; i++)
+        {
+            Debug.Log(name);
+            name = name.Substring(0, 17) + ind.ToString() + name.Substring(18);
+            GetComponent<Renderer>().material.SetTexture("_MainTex", Resources.Load("laika-gif/frame_00_delay-0.1s") as Texture);
+            yield return new WaitForSeconds(0.1f);
+            ind++;
+        }
+    }
+}
---
+++ Space_Race/Assets/Scripts/OnCollisionPlayAudio.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnCollisionPlayAudio : MonoBehaviour {
+
+    private AudioSource audioSource;
+    public AudioClip audio;
+
+    void OnTriggerEnter (Collider collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            Debug.Log("collided play sound");
+            audioSource = GetComponent<AudioSource>();
+            audioSource.clip = audio;
+            audioSource.Play();
+        }
+    }
+}
