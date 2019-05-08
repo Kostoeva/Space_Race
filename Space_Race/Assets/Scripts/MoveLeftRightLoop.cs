0f387972ad5cc99bf158785217d046b6381b8f99
Kostoeva <irmareka@gmail.com>
2019-05-08 00:06:29 -0700

    adding floating script to sputnik

---
+++ Space_Race/Assets/Scripts/MoveLeftRightLoop.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveLeftRightLoop : MonoBehaviour {
+
+    int maxValue = 15; 
+    int minValue = -15; 
+    float currentValue = 0; 
+    int direction = 1;
+
+    // Use this for initialization
+    void Start () {
+        currentValue = transform.position.x;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        currentValue += Time.deltaTime * direction; 
+        if (currentValue >= maxValue)
+        {
+            direction *= -1;
+            currentValue = maxValue;
+        }
+        else if (currentValue <= minValue)
+        {
+            direction *= -1;
+            currentValue = minValue;
+        }
+        transform.position = new Vector3(currentValue, transform.position.y, transform.position.z);
+    }
+}
